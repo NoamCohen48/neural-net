7be5615faaffe4703591d272ba706ef7bd181d25
Noam <cohennoam48@gmail.com>
2023-05-28 15:30:34 +0300

    main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def main(l:list[int]):
-    l.pop()
+from configuration import Configuration
+from csv_manager import read_file
+from neuralNetwork import NeuralNetwork
+
+
+def main():
+    configuration = Configuration(0.05, 20, (3072, 1000, 200, 10), 0.5, "test")
+    model = NeuralNetwork(configuration)
+    X, Y = read_file("train.csv")
+    model.train(X, Y)
 
 
 if __name__ == '__main__':
-    x = [1, 2, 3]
-    main(x)
-    print(x)
+    main()
+
