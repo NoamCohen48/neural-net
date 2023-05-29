da33de4680263e426eec56b2c1852bd6010ed5e4
Noam <cohennoam48@gmail.com>
2023-05-29 18:17:37 +0300

    d4

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from csv_manager import read_file2
 from neuralNetwork import NeuralNetwork
 
 def main():
+    print("version 1.0.0")
     configuration = Configuration(0.001, 300, (3072, 1024, 512, 10), 12, "test")
     model = NeuralNetwork(configuration)
     X, Y = read_file2(sys.argv[1])
