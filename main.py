287d6d1d0f6ed70a490fd170b5cf12ff226e5b27
Noam <cohennoam48@gmail.com>
2023-05-29 13:22:49 +0300

    d

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from neuralNetwork import NeuralNetwork
 def main():
     configuration = Configuration(0.001, 100, (3072, 1024, 512, 10), 12, "test")
     model = NeuralNetwork(configuration)
-    print(os.listdir())
     X, Y = read_file2(sys.argv[1])
     model.train(X, Y)
 
